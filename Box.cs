6bc54794b84f223c5762c189eaaa6333b5f478b3
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-03-25 20:43:15 +0100

    Add project files.

---
+++ Box.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P10CarteLotoV1
+{
+    internal class Box
+    {
+        private int? value;
+        private int column;
+        private int line;
+
+        public int? Value {
+            get => this.value;
+            private set {
+                if (value < 1 || value > 90)
+                {
+                    throw new ArgumentException($"Unallowed box value. Expected a value between 1 (inclusive) and 90 (inclusive). Got : {value}");
+                }
+
+                this.value = value;
+            }
+        }
+
+        public bool HasValue { get => (this.value != null); }
+
+        public int Column
+        {
+            get => this.column;
+            private set
+            {
+                if (value < 1 || value > 9)
+                {
+                    throw new ArgumentException($"Unallowed box column. Expected a value between 1 (inclusive) and 9 (inclusive). Got : {value}");
+                }
+
+                this.column = value;
+            }
+        }
+
+        public int Line {
+            get => this.line;
+            private set
+            {
+                if (value < 1 || value > 3)
+                {
+                    throw new ArgumentException($"Unallowed box line. Expected a value between 1 (inclusive) and 3 (inclusive). Got : {value}");
+                }
+
+                this.line = value;
+            }
+        }
+
+        public bool IsTicked { get; set; }
+
+        public Box(int? value, int column, int line)
+        {
+            this.Value = value;
+            this.Column = column;
+            this.Line = line;
+            this.IsTicked = false;
+        }
+    }
+}
---
+++ Card.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P10CarteLotoV1
+{
+    internal class Card
+    {
+        private readonly Box[] boxes = new Box[15];
+        private readonly string serialNumber = Guid.NewGuid().ToString();
+
+        public Card()
+        {
+            this.GenerateBoxes();
+        }
+
+        /// <summary>
+        /// Displays the card in a simple text format.
+        /// </summary>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Display()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Displays a light version of the card in the format `SerialNumber : "04-34-45 ... 87-90"`.
+        /// </summary>
+        public void DisplayLight()
+        {
+            string message = $"{this.serialNumber} : ";
+
+            for (int i = 0; i < this.boxes.Length; i++)
+            {
+                message += this.boxes.ElementAt(i).Value;
+
+                if (i != this.boxes.Length - 1)
+                {
+                    message += "-";
+                }
+            }
+
+            // TODO : Display the message on the screen.
+        }
+
+        /// <summary>
+        /// Generates the boxes for the card in a way that the card is ergonomic.
+        /// </summary>
+        /// <exception cref="System.NotImplementedException"></exception>
+        private void GenerateBoxes()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
